29a4858b76df0d1405815aae44e7b50fc51b6868
Chibuike Umechuwu <38665978+Comurule@users.noreply.github.com>
2020-05-02 08:36:20 +0100

    some setups

---
+++ app.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const app = express();
+const authRoutes = require("./routes/auth.js");
+ 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(authRoutes);
+
+
+mongoose
+    .connect(
+        "mongodb+srv://comurule:mystatus@comurule-qtddz.mongodb.net/test?retryWrites=true&w=majority",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(result => {
+        console.log("Database connected");
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
---
+++ models/schema.js
@@ -0,0 +1,54 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+//Schema for the users
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    userName = {
+        type: String,
+        required: true
+    },
+    password = {
+        type: String,
+        required: true
+    },
+    userCategory = {
+        type: String,
+        enum: ['student', 'tutor'],
+        required: true
+    },
+    schoolCategory = {
+        type: String,
+        enum: ['pry1', 'pry2','pry3','pry4','pry5','pry6',
+                'jss1', 'jss2', 'jss3', 'sss1', 'sss2', 'sss3'],
+        required: true
+    },
+    subjects ={
+        type: Array,
+    }
+    },
+    {timestamps: true});
+
+
+   
+
+//Schema for the subjects
+const subjectSchema = new Schema({
+    subjectName: {
+        type: String,
+        required: true
+    },
+    subjectCategory: {
+        type: String,
+        enum: ['pry1', 'pry2','pry3','pry4','pry5','pry6',
+                'jss1', 'jss2', 'jss3', 'sss1', 'sss2', 'sss3'],
+        required: true
+    }
+},{timestamps: true});
+
+
+module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Subject', subjectSchema);
