7122233e176617b59c85a73a44e5bc9557f0b9c7
Chibuike Umechuwu <38665978+Comurule@users.noreply.github.com>
2020-05-08 06:53:16 +0100

    hmm

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 const authRoutes = require("./routes/auth");
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
